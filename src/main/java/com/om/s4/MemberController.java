31ba9f19523c7854207a1e866cffa00fbd8908b5
woom <saewoomk88@gmail.com>
2018-12-10 15:05:07 +0900

    dd

---
+++ src/main/java/com/om/s4/MemberController.java
@@ -0,0 +1,10 @@
+package com.om.s4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/member/**")
+public class MemberController {
+
+}
